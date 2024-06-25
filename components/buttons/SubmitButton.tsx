f4cd7c099b3d18cf97891fe5b815ad315f88b477
rmber88 <alex.velze0808@gmail.com>
2024-06-25 03:04:07 -0400

    added comment to SubmitButton.tsx

--- components/buttons/SubmitButton.tsx
+++ components/buttons/SubmitButton.tsx
@@ -1,14 +1,21 @@
+// Import twin.macro for utility-first CSS styling using Tailwind CSS
 import tw from "twin.macro";
+
+// Import necessary types from React for defining prop types
 import { ButtonHTMLAttributes } from "react";
+
+// Import the ButtonSpinner component for displaying a loading spinner
 import { ButtonSpinner } from "./buttonSpinner";
 
+// Define the Props type for SubmitButton component
 type Props = {
-  type?: "submit" | "reset" | "button";
-  title: string;
-  isLoading?: boolean;
-  onClick?: () => void;
-} & ButtonHTMLAttributes<HTMLButtonElement>;
+  type?: "submit" | "reset" | "button"; // Optional prop to specify the button type
+  title: string; // The title prop, a string, for the button's title and aria attributes
+  isLoading?: boolean; // Optional prop to indicate loading state
+  onClick?: () => void; // Optional onClick handler function
+} & ButtonHTMLAttributes<HTMLButtonElement>; // Include all HTML button attributes
 
+// Define and export the SubmitButton component
 export default function SubmitButton({
   title,
   type,
@@ -16,6 +23,7 @@ export default function SubmitButton({
   isLoading,
   ...props
 }: Props) {
+  // Define a handleClick function to handle button clicks
   const handleClick = () => {
     if (onClick) {
       // If onClick is provided, execute it
@@ -31,21 +39,21 @@ export default function SubmitButton({
 
   return (
     <button
-      {...props}
-      onClick={handleClick}
-      type={type}
-      title={title}
-      aria-label={title}
-      aria-labelledby={title}
+      {...props} // Spread any additional props onto the button element
+      onClick={handleClick} // Attach the handleClick function to the onClick event
+      type={type} // Set the button's type attribute
+      title={title} // Set the button's title attribute
+      aria-label={title} // Set the aria-label attribute for accessibility
+      aria-labelledby={title} // Set the aria-labelledby attribute for accessibility
+      // Use twin.macro for applying Tailwind CSS classes with responsive and state-based styles
       tw="text-2xl font-medium bg-blue-500 cursor-pointer w-full
         flex items-center justify-center px-3 py-4
-        text-white-100 rounded-t-lg  border border-gray-800
+        text-white-100 rounded-t-lg border border-gray-800
         transition-all duration-200
         hover:(text-white-100 bg-black-100)
-        disabled:(opacity-50 cursor-not-allowed)
-      "
+        disabled:(opacity-50 cursor-not-allowed)"
     >
-      {isLoading ? ButtonSpinner : title}
+      {isLoading ? ButtonSpinner : title} // Render ButtonSpinner if isLoading is true, otherwise render the title
     </button>
   );
 }
